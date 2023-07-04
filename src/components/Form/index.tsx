e776891c84d91202890576e159a29f48c1ffe416
GuilhermeAFonseca <guilherme.araujo.f@gmail.com>
2023-07-04 08:45:39 -0300

    Inclusão do projeto inicial

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { Text, TextInput, View, TouchableOpacity } from 'react-native';
+import ResultImc from './ResultImc/';
+import styles from './style';
+
+export default function Form() {
+  const [height, setHeight] = useState(null);
+  const [weight, setWeight] = useState(null);
+  const [messageImc, setMessageImc] = useState('preencha o peso e altura');
+  const [imc, setImc] = useState(null);
+  const [textButton, setTextButton] = useState('Calcular');
+
+  function imcCalculator() {
+    const imc = (weight / (height * height)).toFixed(2);
+    return setImc(imc);
+  }
+
+  function validationImc() {
+    if (weight != null && height != null) {
+      imcCalculator();
+      setHeight(null);
+      setWeight(null);
+      setMessageImc('Seu imc é igual: ');
+      setTextButton('Calcular novamente');
+      return;
+    }
+    setImc(null);
+    setTextButton('Calcular');
+    setMessageImc('preencha o peso e altura');
+  }
+  return (
+    <View style={styles.formContext}>
+      <View style={styles.form}>
+        <Text style={styles.formLabel}>Altura</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={setHeight}
+          value={height}
+          placeholder="Ex. 1.75"
+          keyboardType="numeric"
+        />
+        <Text style={styles.formLabel}>Peso</Text>
+        <TextInput
+          style={styles.input}
+          value={weight}
+          onChangeText={setWeight}
+          placeholder="Ex. 90.75"
+          keyboardType="numeric"
+        />
+        <TouchableOpacity
+          style={styles.buttonCalculator}
+          onPress={() => validationImc()}
+        >
+          <Text style={styles.textButtonCalculator}>{textButton}</Text>
+        </TouchableOpacity>
+      </View>
+      <ResultImc messageResultImc={messageImc} resultImc={imc} />
+    </View>
+  );
+}
